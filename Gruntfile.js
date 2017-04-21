0bef247d14eedb7548fc08569a639bf74916312b
Julius Vergara <juliuspvergara@gmail.com>
2017-04-20 21:22:06 -0700

    Add new grunt task to compress images and gitignore to hide unnecessary node modules

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        //Compress images
+        imagemin: {
+            dist: {
+                options: {
+                    optimizeationLevel: 5
+                },
+                files: [{
+                        expand: true,
+                        cwd: 'img',
+                        src: ['**/*.{png,jpg,gif}'],
+                    },
+                    {
+                        expand: true,
+                        cwd: 'views/images',
+                        src: ['**/*.{png,jpg,gif}'],
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            portfolio: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    '../Project-Frontend-Nanodegree-Mobile-Portfolio/index.html': 'index.html',
+                }
+            },
+            pizzas: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    '../Project-Frontend-Nanodegree-Mobile-Portfolio/views/pizza.html': 'views/pizza.html',
+                }
+            }
+        },
+        uglify: {
+            min: {
+                files: grunt.file.expandMapping(['js/*.js', 'views/**/*.js'], '../Project-Frontend-Nanodegree-Mobile-Portfolio/', {
+                    rename: function(destBase, destPath) {
+                        return destBase + destPath;
+                    }
+                })
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                        expand: true,
+                        cwd: 'css',
+                        src: '*.css',
+                        dest: '../diff/css',
+                        ext: '.css'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'views/css',
+                        src: '*.css',
+                        dest: '../Project-Frontend-Nanodegree-Mobile-Portfolio/views/css',
+                        ext: '.css'
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['imagemin', 'htmlmin', 'uglify', 'cssmin']);
+
+};
