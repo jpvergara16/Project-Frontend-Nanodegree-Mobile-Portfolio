31c6a8f3610e0cf2cd4714d440a4d8dbcd2e2582
Julius Vergara <juliuspvergara@gmail.com>
2017-04-27 14:09:05 -0700

    Made new src folder containing unminified files

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,10 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        // Clean destion folder
+        clean: {
+          folder: ['dest']
+        },
         //Compress images
         imagemin: {
     			dist: {
@@ -75,12 +79,12 @@ module.exports = function(grunt) {
             }
         }
     });
-
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['imagemin', 'htmlmin', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['clean','imagemin', 'htmlmin', 'uglify', 'cssmin']);
 
 };
