867cde42a8a8b6dcaba96e7be20ee176fea3be83
Julius Vergara <juliuspvergara@gmail.com>
2017-04-27 14:27:35 -0700

    Refactored grunt task and fixed src issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,122 @@
+module.exports = function (grunt) {
 
-module.exports = function(grunt) {
-
-    // Project configuration.
+    // Initialising
     grunt.initConfig({
+        //Import package.json
         pkg: grunt.file.readJSON('package.json'),
-        // Clean destion folder
-        clean: {
-          folder: ['dest']
-        },
-        //Compress images
-        imagemin: {
-    			dist: {
-    				options: {
-    					optimizeationLevel: 5
-    				},
-    				files: [
-    				{
-    					expand: true,
-    					cwd: 'img',
-    					src: ['**/*.{png,jpg,gif}'],
-    					dest: 'dest/img/'
-    				},
-    				{
-    					expand: true,
-    					cwd: 'views/images',
-    					src: ['**/*.{png,jpg,gif}'],
-    					dest: 'dest/views/images/'
-    				}
-    				]
-    			}
-    		},
-        htmlmin: {
-            portfolio: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    '../Project-Frontend-Nanodegree-Mobile-Portfolio/index.html': 'dest/index.html',
-                }
-            },
-            pizzas: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    '../Project-Frontend-Nanodegree-Mobile-Portfolio//views/pizza.html': 'dest/views/pizza.html',
-                }
-            }
-        },
+
+		// Clean destion folder
+		clean: {
+			folder: ['dest']
+		},
+
+		//Compress images
+		imagemin: {
+			dist: {
+				options: {
+					optimizeationLevel: 5
+				},
+				files: [
+				{
+					expand: true,
+					cwd: 'img',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'dest/img/'
+				},
+				{
+					expand: true,
+					cwd: 'views/images',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'dest/views/images/'
+				}
+				]
+			}
+		},
+        //Minify js files
         uglify: {
-            min: {
-                files: grunt.file.expandMapping(['js/*.js', '/views/**/*.js'], '../Project-Frontend-Nanodegree-Mobile-Portfolio/', {
-                    rename: function(destBase, destPath) {
-                        return destBase + destPath;
-                    }
-                })
+            my_target: {
+                files: {
+					'dest/views/js/main.min.js': ['views/js/main.js'],
+					'dest/js/perfmatters.min.js': ['js/perfmatters.js']
+				}
             }
         },
+
+        //Minify CSS files
         cssmin: {
-            target: {
-                files: [{
-                        expand: true,
-                        cwd: 'css',
-                        src: '*.css',
-                        dest: '..dest/css',
-                        ext: '.css'
-                    },
-                    {
-                        expand: true,
-                        cwd: 'views/css',
-                        src: '*.css',
-                        dest: '../Project-Frontend-Nanodegree-Mobile-Portfolio/dest/views/css',
-                        ext: '.css'
-                    }
-                ]
+            my_target: {
+                files: {
+					'dest/css/print.min.css': ['css/print.css'],
+					'dest/css/style.min.css': ['css/style.css'],
+					'dest/views/css/bootstrap-grid.min.css': ['views/css/bootstrap-grid.css'],
+					'dest/views/css/style.min.css': ['views/css/style.css']
+				}
             }
-        }
+        },
+
+		//Inline minified CSS file
+		//Reference https://github.com/eruizdechavez/grunt-string-replace
+		'string-replace': {
+			inline: {
+				files: {
+					'dest/': ['*.html', 'views/*.html'],
+				},
+				options: {
+					replacements: [
+					{
+						pattern: '<!-- @import main css -->',
+						replacement: '<style><%= grunt.file.read("dest/css/style.min.css") %></style>'
+					},
+					{
+						pattern: '<link href="css/print.css" media="print" rel="stylesheet">',
+						replacement: '<link href="css/print.min.css" media="print" rel="stylesheet">'
+					},
+					{
+						pattern: '<!-- @import Pizza stylesheet -->',
+						replacement: '<style><%= grunt.file.read("dest/views/css/style.min.css") %></style>'
+					},
+					{
+						pattern: '<link rel="stylesheet" href="css/bootstrap-grid.css">',
+						replacement: '<link rel="stylesheet" href="css/bootstrap-grid.min.css">'
+					},
+					{
+						pattern: '<script src="js/perfmatters.js"></script>',
+						replacement: '<script><%= grunt.file.read("dest/js/perfmatters.min.js") %></script>'
+					},
+					{
+						pattern: '<script type="text/javascript" src="js/main.js"></script>',
+						replacement: '<script type="text/javascript" src="js/main.min.js"></script>'
+					}]
+				}
+			}
+		},
+
+		htmlmin: {
+			dest: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'dest/',
+					src: ['*.html', 'views/*.html'],
+					dest: 'dest/',
+					filter: 'isFile'
+				}]
+			}
+		}
     });
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+    // Load Grunt plugins
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-string-replace');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    grunt.registerTask('default', ['clean','imagemin', 'htmlmin', 'uglify', 'cssmin']);
+    // Run tasks
+    grunt.registerTask('default', ['clean', 'imagemin', 'uglify', 'cssmin', 'string-replace', 'htmlmin']);
 
 };
